706a5d5c0649b39de4088bdd035b529fda0a950e
rowpacmal <rowpacmaldev@gmail.com>
2025-10-24 11:56:26 +0200

    chore(config): enhanced configs with path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import legacy from '@vitejs/plugin-legacy';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig(({ mode }) => ({
   base: '',
@@ -33,4 +34,9 @@ export default defineConfig(({ mode }) => ({
       targets: ['defaults', 'not IE 11', 'Android >= 9'],
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 }));
