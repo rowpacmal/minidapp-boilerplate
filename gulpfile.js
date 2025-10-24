2907269c2c4d19e6f7fadfeeef5d5e63a9087ebb
rowpacmal <rowpacmaldev@gmail.com>
2025-10-24 11:45:50 +0200

    refactor(gulpfile): migrated to es module syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
-const gulp = require('gulp');
-const zip = require('gulp-zip');
-const app = require('./package.json');
+import gulp from 'gulp';
+import zip from 'gulp-zip';
+import app from './package.json' with { type: 'json' };
 
-function zipTask() {
+export default function zipTask() {
   return gulp
     .src('build/**', { encoding: false })
     .pipe(zip(`${app.name}-${app.version}.mds.zip`))
     .pipe(gulp.dest('minidapp'));
 }
-
-exports.default = zipTask;
