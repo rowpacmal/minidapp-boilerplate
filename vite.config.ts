2aec4aef487214277dc07dd7b5af0017ddae18e3
rowpacmal <rowpacmaldev@gmail.com>
2025-10-24 10:08:51 +0200

    chore(config): updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import legacy from '@vitejs/plugin-legacy';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  let devEnv = '';
+  let devEnv = '</head>';
   const env = loadEnv(mode, process.cwd(), '');
 
   if (mode === 'development') {
@@ -24,13 +25,17 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       react(),
-      tailwindcss(),
       {
         name: 'inject-script-env',
+        enforce: 'pre',
         transformIndexHtml(html) {
           return html.replace('</head>', devEnv);
         },
       },
+      tailwindcss(),
+      legacy({
+        targets: ['defaults', 'not IE 11', 'Android >= 9'],
+      }),
     ],
   };
 });
