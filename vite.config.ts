161ea03f1daf445ea3e4ae463de92daeb5d00955
rowpacmal <rowpacmaldev@gmail.com>
2025-10-24 10:15:05 +0200

    refactor(config): streamlined vite config + improved dev script injection

--- vite.config.ts
+++ vite.config.ts
@@ -3,39 +3,34 @@ import react from '@vitejs/plugin-react-swc';
 import legacy from '@vitejs/plugin-legacy';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  let devEnv = '</head>';
-  const env = loadEnv(mode, process.cwd(), '');
-
-  if (mode === 'development') {
-    devEnv = `<script>
-      var DEBUG = "${env.VITE_DEBUG}" === 'true';
-      var DEBUG_HOST = "${env.VITE_DEBUG_HOST}";
-      var DEBUG_PORT = "${env.VITE_DEBUG_MDS_PORT}";
-      var DEBUG_MINIDAPPID = "${env.VITE_DEBUG_MINIDAPPID}";
-      var DEBUG_UID = "${env.VITE_DEBUG_UID}";
-    </script></head>`;
-  }
-
-  return {
-    base: '',
-    build: {
-      outDir: 'build',
-    },
-    plugins: [
-      react(),
-      {
-        name: 'inject-script-env',
-        enforce: 'pre',
-        transformIndexHtml(html) {
-          return html.replace('</head>', devEnv);
-        },
+export default defineConfig(({ mode }) => ({
+  base: '',
+  build: {
+    outDir: 'build',
+  },
+  plugins: [
+    react(),
+    {
+      name: 'inject-script-env',
+      enforce: 'pre',
+      transformIndexHtml(html) {
+        if (mode === 'development') {
+          const env = loadEnv(mode, process.cwd(), '');
+          const devScript = `<script>
+            var DEBUG = "${env.VITE_DEBUG}" === 'true';
+            var DEBUG_HOST = "${env.VITE_DEBUG_HOST}";
+            var DEBUG_PORT = "${env.VITE_DEBUG_MDS_PORT}";
+            var DEBUG_MINIDAPPID = "${env.VITE_DEBUG_MINIDAPPID}";
+            var DEBUG_UID = "${env.VITE_DEBUG_UID}";
+          </script></head>`;
+          return html.replace('</head>', devScript);
+        }
+        return html;
       },
-      tailwindcss(),
-      legacy({
-        targets: ['defaults', 'not IE 11', 'Android >= 9'],
-      }),
-    ],
-  };
-});
+    },
+    tailwindcss(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'Android >= 9'],
+    }),
+  ],
+}));
